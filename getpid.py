24fb258cb6b6731d7815484f9214103092ae1335
Alessandro Vivas <alessandro.vivas@gmail.com>
2021-02-19 08:51:22 -0300

    Create getpid.py

---
+++ getpid.py
@@ -0,0 +1,10 @@
+
+# importar a biblioteca
+import os
+
+print("Obtendo o PID do Processo")
+print("PID {}".format(os.getpid()))
+
+print("Obtendo o PPID do Processo")
+print("PPID {}".format(os.getppid()))
+
